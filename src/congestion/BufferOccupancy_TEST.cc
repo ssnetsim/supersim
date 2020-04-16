d3b2de8dcd2e0d3e2febbb9759d287f63041907a
Nic McDonald <n.mcdonald83@gmail.com>
2020-04-16 14:06:00 -0700

    Modified CongestionSensor to allow for dynamic buffer allocation

--- src/congestion/BufferOccupancy_TEST.cc
+++ src/congestion/BufferOccupancy_TEST.cc
@@ -231,14 +231,11 @@ TEST(BufferOccupancy, normPort) {
   for (u32 port = 0; port < numPorts; port++) {
     for (u32 vc = 0; vc < numVcs; vc++) {
       // check credits
-      u32 curSum = 0;
-      u32 maxSum = 0;
+      f64 sum = 0.0;
       for (u32 vc2 = 0; vc2 < numVcs; vc2++) {
-        u32 max = port * 10 + vc2 + 2;
-        curSum += 2;
-        maxSum += max;
+        sum += 2.0 / (port * 10 + vc2 + 2);
       }
-      f64 exp = (f64)curSum / (f64)maxSum;
+      f64 exp = sum / numVcs;
       check.setEvent(time, 0, 0, 0, port, vc, exp);
     }
   }
@@ -319,7 +316,7 @@ TEST(BufferOccupancy, absPort) {
       for (u32 vc2 = 0; vc2 < numVcs; vc2++) {
         curSum += 2;
       }
-      f64 exp = (f64)curSum;
+      f64 exp = (f64)curSum / numVcs;
       check.setEvent(time, 0, 0, 0, port, vc, exp);
     }
   }
